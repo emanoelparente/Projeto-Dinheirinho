fc5e83c3b261a5b1ad1b78e98e306f41447f562a
Emanoel Parente <106496107+emanoelparente@users.noreply.github.com>
2024-04-19 22:31:07 -0300

    Acessos usuario
    Filtragem de lançamentos por usuário

--- projeto-din-v2/app.js
+++ projeto-din-v2/app.js
@@ -15,7 +15,8 @@ const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    queryTimeout:60000
 });
 
 const publicDirectory = path.join(__dirname, './public');
@@ -41,6 +42,7 @@ module.exports = db;
 
 //Define Routes
 app.use('/', require('./routes/pages'));
+
 app.use('/auth', require('./routes/auth'));
 
 app.listen(5000, () => {
